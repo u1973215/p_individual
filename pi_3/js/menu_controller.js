fe96069fab8607f198a89b2b0ec709e9b5bf9cc3
u1973215 <u1973215@campus.udg.edu>
2022-03-14 18:43:57 +0100

    pi3 [1,5) and css tweaks
    Del apartat 1 al 5, actualment fent el sistema de dificultat.

---
+++ pi_3/js/menu_controller.js
@@ -0,0 +1,21 @@
+function start_game(){
+	name = prompt("User name");
+	
+	sessionStorage.setItem("username", name);
+	
+	loadpage("./html/game.html");
+}
+
+function exit (){
+	if (name != ""){
+		alert("Leaving " + name + "'s game");
+	}
+	name = "";
+	loadpage("../"); // Return to Practiques menu
+}
+
+function options(){
+	loadpage("./html/options.html");
+}
+
+
